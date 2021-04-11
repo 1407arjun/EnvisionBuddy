6da89813683dea257bc9daba3d93082d4bb3c6d1
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2021-04-11 06:01:57 +0530

    Updated ML model URL

--- app/src/main/java/com/teaminversion/envisionbuddy/ChoiceActivity.java
+++ app/src/main/java/com/teaminversion/envisionbuddy/ChoiceActivity.java
@@ -73,7 +73,7 @@ public class ChoiceActivity extends AppCompatActivity {
         AnalyzeTextTask analyzeTextTask = new AnalyzeTextTask();
         try {
             String urlEncoder = URLEncoder.encode(inputText, "UTF-8");
-            analyzeTextTask.execute("https://5592c62503e2.ngrok.io/" + urlEncoder);
+            analyzeTextTask.execute("https://d4099e48fea8.ngrok.io/" + urlEncoder);
             progress = new ProgressDialog(this);
             progress.setMessage("Retrieving data");
             progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
